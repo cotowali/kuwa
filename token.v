ab9f871175788f547cf194a1ffeae20a623ecd58
zakuro <z@kuro.red>
2024-02-26 08:46:46 +0900

    update license header

--- token.v
+++ token.v
@@ -1,4 +1,6 @@
-// Copyright 2022 The Kuwa Authors.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2023-2024 zakuro <z@kuro.red> (https://x.com/zakuro9715)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 module kuwa
 
 pub struct Token {
